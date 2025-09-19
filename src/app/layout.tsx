e35ffb65161c81724a32cc58d4fd3f1aa83e40ea
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-09-20 07:33:22 +1000

    unauth sidebar complete, posting user data to console not held by clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -43,7 +44,7 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="midden lg:block lg:col-span-3">
-                      sidebar
+                      <Sidebar />
                     </div>
                     <div className="lg:col-span-9">
                       {children}
