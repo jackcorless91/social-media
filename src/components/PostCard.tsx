a7261dc4e7134887e1cebcf5b499b0f3d8ed27a1
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-09-22 06:36:29 +1000

    post actons toggle like func created, post logic start

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,11 +2,40 @@
 
 import {Post} from "@prisma/client";
 import {useState} from "react";
+import {getPosts} from "@/actions/post.action";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>
+type Post = Posts[number]
 
 function PostCard({ post, dbUserId } : { post: Post; dbUserId: string | null }) {
   const { user } = newUser();
   const [newComment, setNewComment] = useState("");
   const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(post.likes.some(like => like.userId === dbUserId));
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes); //no loading state - update likes now
+
+  const handleLike = async () => {
+    if (isLiking) return;
+
+    try {
+      setIsLiking(true)
+      setHasLiked(prev => !prev)
+      setOptimisticLikes(prev => prev + (hasLiked ? -1 : 1))
+      await toggleLike(post.id)
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(like => like.userId === dbUserId))
+    } finally {
+      setIsLiking(false)
+    }
+  }
+
+  const handleAddComment = async () => {}
+
+  const handleDeletePost = async () => {}
+
   return <div>PostCard</div>
 }
 
