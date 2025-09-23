25fa573abef50f2d6e9751869bd641468b3557e7
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-09-24 07:42:31 +1000

    config @profile

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,26 @@
+import {getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing} from "@/actions/profile.action";
+import {notFound} from "next/navigation";
+
+export async function generateMetaData({ params }: {params: { username: string } }) {
+  const user = await getProfileByUsername(params.username)
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
+
 async function ProfilePage({ params }: {params: { username: string } }) {
+  const user = await getProfileByUsername(params.username)
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+      getUserPosts(user.id),
+      getUserLikedPosts(user.id),
+      isFollowing(user.id),
+  ])
 
   await new Promise((resolve) => setTimeout(resolve, 3000))
   return <div>ProfilePage</div>
