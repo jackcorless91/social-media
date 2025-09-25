4ccfa7b3f9b4bbcc7d587958c56d201312833482
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-09-26 07:26:49 +1000

    image posting implemented and working

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -9,6 +9,8 @@ import {useUser} from "@clerk/nextjs";
 import {createPost} from "@/actions/post.action";
 import {ImageIcon, Loader2Icon, SendIcon} from "lucide-react";
 import toast from "react-hot-toast";
+import ImageUpload from "@/components/ImageUpload";
+import {image} from "@uploadthing/mime-types/image";
 
 export default function CreatePost() {
   const { user } = useUser();
@@ -55,7 +57,18 @@ export default function CreatePost() {
                   disabled={isPosting}
               />
             </div>
-            {/*handle images here*/}
+
+            {(showImageUpload || imageUrl) && (
+                <div className="border rounded-lg" p-4>
+                  <ImageUpload
+                  endpoint="postImage"
+                  value={imageUrl}
+                  onChange={(url) => {
+                      setImageUrl(url);
+                      if (!url) setShowImageUpload(false);
+                  }}/>
+                </div>
+            )}
 
             <div className="flex items-center justify-between border-t pt-4">
               <div className="flex space-x-2">
